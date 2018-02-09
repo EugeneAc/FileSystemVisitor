48d1565efcc8f467cb6ef215b7a56bd775a03f4c
qwerty11aa <asdnvx14@yahoo.com>
2018-02-09 11:22:30 +0600

    1
    Corrected

--- FileSystemVisitor/Program.cs
+++ FileSystemVisitor/Program.cs
@@ -12,21 +12,11 @@ namespace FileSystemVisitor
     {
         static void Main(string[] args)
         {
-            int?[] myarray = new int?[5] { null, 2, 3, 4, 5 };
-            Console.WriteLine(Get2ndMaxNumber(myarray));
-            foreach (var el in myarray)
-            {
-                Console.WriteLine(el);
-            }
 
-            var worker = new BCLWorker();
-            worker.Progress += worker_Progress;
-            worker.DoWork();
-            Console.WriteLine("");
             var visor = new FileSystemVisitor(@"d:\Test", name => name.Contains("txt"));
-            visor.Start += (s,e)=> Console.WriteLine("Started");
+            visor.Start += (s, e) => Console.WriteLine("Started");
             visor.Finish += (s, e) => Console.WriteLine("Finished");
-            visor.FileFinded += (s, e) => Console.WriteLine("File Finded "+e.FoundedName);
+            visor.FileFinded += (s, e) => Console.WriteLine("File Finded " + e.FoundedName);
             visor.DirectoryFinded += (s, e) => Console.WriteLine("Dir Finded " + e.FoundedName);
             visor.FilteredDirectoryFinded += (s, e) => Console.WriteLine("Filtered Dir Finded " + e.FoundedName);
             visor.FilteredFileFinded += (s, e) =>
@@ -34,7 +24,7 @@ namespace FileSystemVisitor
                 var sender = (FileSystemVisitor)s;
                 Console.WriteLine("Filtered File Finded " + e.FoundedName);
                 if (e.FoundedName.Contains("1"))
-                { 
+                {
                     sender.StopSerach = true;
                     Console.WriteLine("!");
                 }
@@ -48,75 +38,5 @@ namespace FileSystemVisitor
             Console.ReadLine();
         }
 
-        static private void worker_Progress(object sender, BCLWorker.WorkerProgressArgs e)
-        {
-            Console.WriteLine(e.ProgressPercent);
-        }
-
-        static int? Get2ndMaxNumber(int?[] inarray)
-        {
-            try
-            {
-                var internalarray = inarray;
-                int maxindex = Array.IndexOf(internalarray, inarray.Max());
-                internalarray[maxindex] = null;
-                throw new NullReferenceException();
-                return internalarray.Max();
-            }
-            catch (Exception)
-            {
-                return 1;
-            }
-            finally
-            {
-                var internalarray = inarray;
-                int maxindex = Array.IndexOf(internalarray, inarray.Max());
-                internalarray[maxindex] = null;
-                
-            }
-           
-        }
-    }
-
-    public class BCLWorker
-    {
-        public class WorkerProgressArgs
-        {
-            public int ProgressPercent { get; internal set; }
-        }
-
-        public event EventHandler<WorkerProgressArgs> Progress;
-
-        protected virtual void OnProgress(WorkerProgressArgs args)
-        {
-            Progress?.Invoke(this, args);
-        }
-
-        public void DoWork()
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                OnProgress(
-                    new WorkerProgressArgs { ProgressPercent = i * 10 });
-                System.Threading.Thread.Sleep(100);
-            }
-        }
-    }
-
-    [TestClass]
-    public class BCLEvents
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            var worker = new BCLWorker();
-            worker.Progress += worker_Progress;
-            worker.DoWork();
-        }
-
-        private void worker_Progress(object sender, BCLWorker.WorkerProgressArgs e)
-        {
-            Console.WriteLine(e.ProgressPercent);
-        }
     }
 }
